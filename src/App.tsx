ee440cbe37476c060eb66156bc02adc5a0e33971
dtpvy <dtpvy0202@gmail.com>
2023-11-27 17:33:51 +0700

    add ver-1

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import { RouterProvider } from 'react-router-dom';
-import { router } from '@/configs/routes';
-
-function App() {
-  return <RouterProvider router={router} />;
-}
-
-export default App;
